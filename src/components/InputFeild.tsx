22a0e965d75fb1365b13a5c82ab565859bc436a8
chidiebereojingwa <ojingwaoji@gmail.com>
2022-03-24 22:41:33 +0100

    Add styles to input field

--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import "./style.css"
 
 const InputFeild = () => {
   return (
     <form className='input'>
-        <input type="input" placeholder="Enter a task" className="input_box"/>        
+        <input type="input" placeholder="Enter a task" className="input_box"/>  
+        <button className='input_submit'type='submit'>Go</button>      
     </form>
   )
 }
