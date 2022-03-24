35ca962bbcddbac0e43223865ff0a62a31885fc6
chidiebereojingwa <ojingwaoji@gmail.com>
2022-03-24 22:17:18 +0100

    Add input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import InputFeild from './components/InputFeild';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <span className='heading'>Taskify</span>
+      <InputFeild/>
     </div>
   );
 }
---
+++ src/components/InputFeild.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputFeild = () => {
+  return (
+    <form className='input'>
+        <input type="input" placeholder="Enter a task" className="input_box"/>        
+    </form>
+  )
+}
+
+export default InputFeild
