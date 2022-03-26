0e4d3e91f44ecd5ecf5748df240ba8bcfa0c1ce5
chidiebereojingwa <ojingwaoji@gmail.com>
2022-03-26 09:56:00 +0100

    Update inputFeild.tsx

--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -9,7 +9,7 @@ interface Props {
 const InputFeild: React.FC<Props> = ({todo, setTodo}) => {
   return (
     <form className='input'>
-        <input type="input" placeholder="Enter a task" className="input_box"/>  
+        <input type="input" value={todo} onchange={(e) => setTodo(e.target.value)} placeholder="Enter a task" className="input_box"/>  
         <button className='input_submit'type='submit'>Go</button>      
     </form>
   )
